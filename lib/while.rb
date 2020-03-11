2aab7a94da38024127f7bc126fcde6f1e26b3cb4
Cathy D'Onofrio <catd825@gmail.com>
2020-03-11 02:19:15 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,12 +1,11 @@
 def using_while
     levitation_force = 6
 	while levitation_force < 10
-    puts
+
     counter += 1
   end
   until levitation_force == 10
     puts "Wingardium Leviosa"
-    counter += 1
   end
 end
 
